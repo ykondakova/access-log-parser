24e3b1d69fb5bfc587b9cfd1bc9519a3f5fe580a
Julia Kondakova <git.user.ykondakova@nota.tech>
2025-04-17 18:50:41 +0300

    initial

---
+++ src/main/java/AccessLogParserPackage/Main.java
@@ -0,0 +1,9 @@
+package AccessLogParserPackage;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Случайное число от 0 до 1: " + Math.random());
+
+    }
+}
