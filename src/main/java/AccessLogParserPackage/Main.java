2d8425c478aef65967e342b7e6a9cf49b85682ea
Julia Kondakova <git.user.ykondakova@nota.tech>
2025-04-24 12:43:06 +0300

    Добавлен расчет суммы, разности, произведения и частного двух чисел

--- src/main/java/AccessLogParserPackage/Main.java
+++ src/main/java/AccessLogParserPackage/Main.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.println("Сумма: " + (firstNumber + secondNumber));
+        System.out.println("Разность: " + (firstNumber - secondNumber));
+        System.out.println("Произведение: " + firstNumber * secondNumber);
+        System.out.println("Частное: " + (double)firstNumber / secondNumber);
     }
 }
