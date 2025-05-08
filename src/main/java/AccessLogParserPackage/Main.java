c3a6d60691d684161c29a46e706cfbfb72d8bcfb
Julia Kondakova <git.user.ykondakova@nota.tech>
2025-05-08 22:15:01 +0300

    Добавлено чтение файла в бесконечном цикле

--- src/main/java/AccessLogParserPackage/Main.java
+++ src/main/java/AccessLogParserPackage/Main.java
@@ -1,10 +1,25 @@
 package AccessLogParserPackage;
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+
+        int numberOfFiles = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExist || isDirectory) {
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке");
+                continue;
+            }
+            numberOfFiles++;
+            System.out.println("Путь указан верно" + "\n" + "Это файл номер " + numberOfFiles);
+        }
     }
 }
