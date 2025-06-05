9614d67858ee69e0f21fe2ad4bcae21693d70bf7
Julia Kondakova <git.user.ykondakova@nota.tech>
2025-06-05 18:16:51 +0300

    Добавлен расчет долей запросов от YandexBot и Googlebot

--- src/main/java/AccessLogParserPackage/Main.java
+++ src/main/java/AccessLogParserPackage/Main.java
@@ -1,4 +1,5 @@
 package AccessLogParserPackage;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -10,6 +11,9 @@ public class Main {
     public static void main(String[] args) {
 
         int numberOfFiles = 0;
+        int yandexBotCount = 0;
+        int googleBotCount = 0;
+
         while (true) {
             System.out.println("Введите путь к файлу");
             String path = new Scanner(System.in).nextLine();
@@ -33,28 +37,63 @@ public class Main {
                 while ((line = reader.readLine()) != null) {
                     if (line.length() > 1024) throw new LineTooLongException();
                     linesLength.add(line.length());
+                    String[] parts = line.split(" ");
+                    String ip = parts[0];
+                    String missingProp1 = parts[1];
+                    String missingProp2 = parts[2];
+                    String dateTime = parts[3] + parts[4];
+                    String url = parts[5] + " " + parts[6] + " " + parts[7];
+                    String httpCode = parts[8];
+                    String dataSize = parts[9];
+                    String referer = parts[10];
+                    String userAgent = "";
+                    int i = 11;
+                    for (; i < parts.length - 1; i++) {
+                        userAgent += parts[i] + " ";
+                    }
+                    userAgent += parts[i];
+
+                    String firstBracketsUA = "";
+
+                    int startBracket = line.indexOf('(');
+                    int endBracket = line.indexOf(')', startBracket);
+
+                    if (startBracket != -1 && endBracket != -1 && startBracket < endBracket) {
+                        firstBracketsUA = line.substring(startBracket + 1, endBracket);
+                    }
+
+                    String fragment = "";
+
+                    String[] partsFirstBracketsUA = firstBracketsUA.split(";");
+                    if (partsFirstBracketsUA.length >= 2) {
+                        fragment = partsFirstBracketsUA[1];
+                    }
+
+                    String programSender = fragment.replace(" ", "");
+
+                    int endSlash = programSender.indexOf('/');
+
+                    if (endSlash != -1) {
+                        programSender = programSender.substring(0, endSlash);
+                    }
+
+
+                    if (programSender.equals("YandexBot")) {
+                        yandexBotCount++;
+                    }
+                    if (programSender.equals("Googlebot")) {
+                        googleBotCount++;
+                    }
+
                 }
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
 
-            int maxLength = linesLength.get(0);
-            int minLength = linesLength.get(0);
-
-            for (Integer i: linesLength) {
-                if (i > maxLength) {
-                    maxLength = i;
-                }
-                if (i < minLength) {
-                    minLength = i;
-                }
-            }
-
             System.out.println("Общее количество строк в файле: " + linesLength.size());
-            System.out.println("Длина самой длинной строки в файле: " + maxLength);
-            System.out.println("Длина самой короткой строки в файле: " + minLength);
+            System.out.println("Доля запросов от YandexBot: " + (double)yandexBotCount/linesLength.size());
+            System.out.println("Доля запросов от Googlebot: " + (double)googleBotCount/linesLength.size());
 
         }
-
     }
 }
